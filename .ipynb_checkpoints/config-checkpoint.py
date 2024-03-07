c98d91d2974dadef38090164b282d3c2390f9eab
ubaidb936 <ubaidb936@gmail.com>
2024-03-07 21:10:39 +0000

    changing zephr

--- .ipynb_checkpoints/config-checkpoint.py
+++ .ipynb_checkpoints/config-checkpoint.py
@@ -1,7 +1,3 @@
-## Set pretrained llm model id 
-model_id = "mistralai/Mistral-7B-v0.1"
-
-
 ## Set pdf path (------------Required)
-pdfPath = "pdfs/StockInvesting.pdf"
+pdfPath = "pdfs/ProfessorMagdyBio.pdf"
         
